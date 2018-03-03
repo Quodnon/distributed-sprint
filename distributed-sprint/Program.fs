05a6fb24ebfdf81c5f6740aa74c0805c867151ae
Quodnon <feanorfeanturi@gmail.com>
2018-03-03 02:10:02 +0200

    f sharp merge sort setup of silos and grains

--- distributed-sprint/Program.fs
+++ distributed-sprint/Program.fs
@@ -1,65 +1,36 @@
-﻿namespace basic_task
-// Learn more about F# at http://fsharp.org
+﻿// Learn more about F# at http://fsharp.org
+
 open System
 
-type Game = {
-    Name: string
-    Platform: string
-    Score: int option
-}
-module first_task= 
 [<EntryPoint>]
 let main argv =
-    let stockData =
-        [ "Date,Open,High,Low,Close,Volume,Adj Close";
-        "2012-03-30,32.40,32.41,32.04,32.26,31749400,32.26";
-        "2012-03-29,32.06,32.19,31.81,32.12,37038500,32.12";
-        "2012-03-28,32.52,32.70,32.04,32.19,41344800,32.19";
-        "2012-03-27,32.65,32.70,32.40,32.52,36274900,32.52";
-        "2012-03-26,32.19,32.61,32.15,32.59,36758300,32.59";
-        "2012-03-23,32.10,32.11,31.72,32.01,35912200,32.01";
-        "2012-03-22,31.81,32.09,31.79,32.00,31749500,32.00";
-        "2012-03-21,31.96,32.15,31.82,31.91,37928600,31.91";
-        "2012-03-20,32.10,32.15,31.74,31.99,41566800,31.99";
-        "2012-03-19,32.54,32.61,32.15,32.20,44789200,32.20";
-        "2012-03-16,32.91,32.95,32.50,32.60,65626400,32.60";
-        "2012-03-15,32.79,32.94,32.58,32.85,49068300,32.85";
-        "2012-03-14,32.53,32.88,32.49,32.77,41986900,32.77";
-        "2012-03-13,32.24,32.69,32.15,32.67,48951700,32.67";
-        "2012-03-12,31.97,32.20,31.82,32.04,34073600,32.04";
-        "2012-03-09,32.10,32.16,31.92,31.99,34628400,31.99";
-        "2012-03-08,32.04,32.21,31.90,32.01,36747400,32.01";
-        "2012-03-07,31.67,31.92,31.53,31.84,34340400,31.84";
-        "2012-03-06,31.54,31.98,31.49,31.56,51932900,31.56";
-        "2012-03-05,32.01,32.05,31.62,31.80,45240000,31.80";
-        "2012-03-02,32.31,32.44,32.00,32.08,47314200,32.08";
-        "2012-03-01,31.93,32.39,31.85,32.29,77344100,32.29";
-        "2012-02-29,31.89,32.00,31.61,31.74,59323600,31.74"; ]
-
-    let splitCommas (x:string) =
-        x.Split([|','|])
-    
-    let mapDiff ([|dateS:string; openP:string; highP:string; LowP:string; closeP:string; s:string;a:string;|]) =
-        let diff = abs ((Double.Parse openP) - (Double.Parse closeP))
-        let result =  (dateS, diff) 
-        result
-    
-
-    let result = List.map (splitCommas>>mapDiff) stockData.Tail 
-                        |>List.maxBy snd
-                        |> fst
-
+    let countSplitInv (left: 'a array) (right: 'a array) ar_length =
+        let result = Array.zeroCreate<'a> ar_length
+        let leftlen = left.Length
+        let mutable left_ind, right_ind= 0,0
+        let mutable inv_count = 0L
+        for k = 1 to ar_length-1 do
+            if left_ind >= leftlen then 
+                result.[k] <- right.[right_ind]; right_ind<-right_ind+1
+            elif right_ind > right.Length then 
+                result.[k] <- left.[left_ind]; left_ind<-left_ind+1
+            elif left.[left_ind] < right.[right_ind] then 
+                result.[k] <- left.[left_ind]; left_ind<-left_ind + 1  
+            else result.[k] <- right.[right_ind]; right_ind <- right_ind + 1; inv_count <- inv_count + int64(leftlen - left_ind)
         
-        
-    
-    let chronoTrigger = { Name = "Chrono Trigger"; Platform = "SNES"; Score = Some 5 }
-    let halo = { Name = "Halo"; Platform = "Xbox"; Score = None }
+        result,inv_count
 
-    let decideOn game =
-        game.Score
-        |> Option.map (fun score -> if score > 3 then "play it" else "don't play")
-    
-    decideOn chronoTrigger |>printf "%A"
-    printfn "%A" result
-    decideOn halo |>printf "%A"
+    let rec sortAndCount (arr:'a array) = 
+        match arr.Length with
+        | 0 | 1 -> arr, 0L
+        | n -> 
+                let (b,x) = sortAndCount (arr.[0..n/2-1])
+                let (c,y) = sortAndCount(arr.[n/2..n-1])
+                let (d,z) = countSplitInv b c n
+
+                d, z+x+y
+
+    let testList = [|1;3;5;2;4;6|]
+    let result= testList |> sortAndCount
+    printfn "Hello World from \n %A!" result
     0 // return an integer exit code
